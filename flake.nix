272e6bc3d5269ad137825c9f4a7cfe13b1ff6276
Tobias Abenius <tabenius@users.noreply.github.com>
2026-09-16 23:53:25 +0200

    fix: keep shellcheck invocation valid

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
           shell = pkgs.runCommand "rebekah-shell-checks" {
             nativeBuildInputs = [ pkgs.shellcheck ];
           } ''
-            shellcheck --severity=warning \\\n              ${./nix/entrypoint.sh} \\\n              ${./nix/ephor-connector.sh} \\\n              ${./tests/ephor-connector.sh} \\\n              ${./tests/smoke.sh}
+            shellcheck --severity=warning ${./nix/entrypoint.sh} ${./nix/ephor-connector.sh} ${./tests/ephor-connector.sh} ${./tests/smoke.sh}
             touch $out
           '';
           inherit (self.packages.${system}) weftmark sylvae;
